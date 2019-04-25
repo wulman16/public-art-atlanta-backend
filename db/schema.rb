f74f004f30ecb948a257dbcfd7f60d58a09df630
wulman16 <will.ulman@gmail.com>
2019-04-25 16:24:38 -0400

    remove seen and favorite from user_artworks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_25_133530) do
+ActiveRecord::Schema.define(version: 2019_04_25_202407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,8 +33,6 @@ ActiveRecord::Schema.define(version: 2019_04_25_133530) do
   create_table "user_artworks", force: :cascade do |t|
     t.integer "user_id"
     t.integer "artwork_id"
-    t.boolean "seen"
-    t.boolean "favorite"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
