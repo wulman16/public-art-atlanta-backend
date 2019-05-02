01c2c8838c2b6856f2be4bed7a609ab2f352f123
wulman16 <will.ulman@gmail.com>
2019-05-02 11:14:39 -0400

    write create controller method for artworks

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -10,10 +10,24 @@ class ArtworksController < ApplicationController
     render json: @artwork.artwork_json, status: :ok
   end
 
+  def create
+    @artwork = Artwork.new(artwork_params)
+    if @artwork.save
+      render json: @artwork, status: :created
+    else
+      render json: { errors: @artwork.errors.full_messages }, status: :unprocessible_entity
+    end
+  end
+
   private
 
   def find_artwork
     @artwork = Artwork.find(params[:id])
+  end
+
+  def artwork_params
+    params.permit(:title, :artist, :year, :image, :medium,
+      :owner, :profile, :lat, :lng, :source)
   end
 
 end
