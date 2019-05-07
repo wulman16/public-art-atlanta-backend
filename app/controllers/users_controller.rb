413208b801232b2c5b1ec0a8cd10144e911d6592
wulman16 <will.ulman@gmail.com>
2019-05-07 10:46:51 -0400

    fix signup token

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user && @user.valid?
       @user.save
-      token = encode_token(user_id: @user.id)
+      @token = encode_token(user_id: @user.id)
       render json: {user: @user, jwt: @token}, status: :created
     else
       render json: { errors: @user.errors.full_messages }, status: :not_acceptable
